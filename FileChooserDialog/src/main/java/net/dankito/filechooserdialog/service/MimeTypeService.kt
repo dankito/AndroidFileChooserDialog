f54b1ae13097185ffae5ecb938fb1cf61150b5ef
dankl <dankl@comfylight.com>
2018-02-08 00:55:17 +0100

    Added MimeTypeService

---
+++ FileChooserDialog/src/main/java/net/dankito/filechooserdialog/service/MimeTypeService.kt
@@ -0,0 +1,26 @@
+package net.dankito.filechooserdialog.service
+
+import android.webkit.MimeTypeMap
+import java.io.File
+
+
+class MimeTypeService {
+
+    fun getMimeType(file: File): String? {
+        file.extension?.let { extension ->
+            return MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension)
+        }
+
+        return null
+    }
+
+
+    fun isImageType(mimeType: String): Boolean {
+        return mimeType.startsWith("image")
+    }
+
+    fun isVideoType(mimeType: String): Boolean {
+        return mimeType.startsWith("video")
+    }
+
+}
