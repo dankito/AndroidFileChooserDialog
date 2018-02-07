cd67647ed6f23afd61c9323eb8156faaea01886b
dankl <dankl@comfylight.com>
2018-02-07 19:47:17 +0100

    Added DirectoryContentViewHolder and

---
+++ FileChooserDialog/src/main/java/net/dankito/filechooserdialog/ui/adapter/viewholder/DirectoryContentViewHolder.kt
@@ -0,0 +1,13 @@
+package net.dankito.filechooserdialog.ui.adapter.viewholder
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.widget.TextView
+import kotlinx.android.synthetic.main.list_item_file.view.*
+
+
+class DirectoryContentViewHolder(fileListItemView: View) : RecyclerView.ViewHolder(fileListItemView) {
+
+    val txtFilename: TextView = fileListItemView.txtFilename
+
+}
