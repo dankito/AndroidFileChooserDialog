b0421d8651640c2fdf7a61e23f09298a1894c751
dankl <dankl@comfylight.com>
2018-02-07 22:49:45 +0100

    Implemented showing parent directories in toolbar

--- FileChooserDialog/src/main/java/net/dankito/filechooserdialog/FileChooserDialog.kt
+++ FileChooserDialog/src/main/java/net/dankito/filechooserdialog/FileChooserDialog.kt
@@ -3,7 +3,12 @@ package net.dankito.filechooserdialog
 import android.os.Environment
 import android.support.v4.app.FragmentManager
 import android.view.View
+import android.view.ViewGroup
+import android.widget.HorizontalScrollView
+import android.widget.LinearLayout
+import android.widget.ScrollView
 import kotlinx.android.synthetic.main.dialog_file_chooser.view.*
+import kotlinx.android.synthetic.main.view_parent_directory.view.*
 import net.dankito.filechooserdialog.ui.adapter.DirectoryContentAdapter
 import net.dankito.filechooserdialog.ui.dialog.FullscreenDialogFragment
 import java.io.File
@@ -18,8 +23,17 @@ class FileChooserDialog : FullscreenDialogFragment() {
 
     private val adapter = DirectoryContentAdapter()
 
+    private lateinit var parentDirectoriesScrollView: HorizontalScrollView
+
+    private lateinit var parentDirectoriesLayout: LinearLayout
+
 
     override fun setupUI(rootView: View) {
+        parentDirectoriesScrollView = rootView.scrParentDirectoriesView
+        parentDirectoriesLayout = rootView.lytParentDirectoriesView
+
+        rootView.scrParentDirectoriesView.isHorizontalScrollBarEnabled = true
+
         rootView.rcyCurrentDirectoryContent.adapter = adapter
         adapter.itemClickListener = { file -> fileClicked(file) }
 
@@ -28,7 +42,36 @@ class FileChooserDialog : FullscreenDialogFragment() {
 
 
     private fun currentDirectoryChanged(directory: File) {
+        showParentDirectories(directory)
         showContentForDirectory(directory)
+    }
+
+    private fun showParentDirectories(directory: File) {
+        parentDirectoriesLayout.removeAllViews()
+
+        addParentDirectoriesRecursively(directory)
+
+        parentDirectoriesScrollView.post { // wait till new size is calculated
+            parentDirectoriesScrollView.fullScroll(ScrollView.FOCUS_RIGHT)
+        }
+    }
+
+    private fun addParentDirectoriesRecursively(directory: File?) {
+        if(directory != null && directory.isDirectory && directory.parentFile != null) { // parent.parentFile != null: filter out root
+            addParentDirectoriesRecursively(directory.parentFile)
+
+            addParentDirectoryView(directory)
+        }
+    }
+
+    private fun addParentDirectoryView(parent: File) {
+        val parentDirectoryView = layoutInflater.inflate(R.layout.view_parent_directory, null)
+        parentDirectoryView.txtDirectoryName.text = parent.name
+
+        parentDirectoriesLayout.addView(parentDirectoryView)
+        parentDirectoryView.layoutParams?.height = ViewGroup.LayoutParams.MATCH_PARENT
+
+        parentDirectoryView.setOnClickListener { currentDirectoryChanged(parent) }
     }
 
     private fun showContentForDirectory(directory: File) {
