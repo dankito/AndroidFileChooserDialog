2732d0c4c9da0b6a5fe54c5c68e0322f21648f08
dankl <dankl@comfylight.com>
2018-02-08 15:32:41 +0100

    Added a DrawerLayout with folder shortcuts

--- FileChooserDialog/src/main/java/net/dankito/filechooserdialog/FileChooserDialog.kt
+++ FileChooserDialog/src/main/java/net/dankito/filechooserdialog/FileChooserDialog.kt
@@ -1,7 +1,14 @@
 package net.dankito.filechooserdialog
 
+import android.os.Build
 import android.os.Environment
+import android.support.design.widget.NavigationView
 import android.support.v4.app.FragmentManager
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.widget.Toolbar
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import kotlinx.android.synthetic.main.dialog_file_chooser.view.*
@@ -25,6 +32,8 @@ class FileChooserDialog : FullscreenDialogFragment() {
     private lateinit var directoryContentView: DirectoryContentView
 
     private lateinit var btnSelect: Button
+
+    private lateinit var drawerLayout: DrawerLayout
 
 
     private lateinit var dialogType: FileChooserDialogType
@@ -52,14 +61,41 @@ class FileChooserDialog : FullscreenDialogFragment() {
         btnSelect = rootView.btnSelect
         btnSelect.setOnClickListener { selectingFilesDone() }
 
+        setupNavigationMenu(rootView.navigationView)
+
         setCurrentDirectory(Environment.getExternalStorageDirectory())
+    }
+
+    private fun setupNavigationMenu(navigationView: NavigationView) {
+        navigationView.menu?.findItem(R.id.navFolderShortcutDocuments)?.isVisible = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT // Documents folder is only available on KitKat and newer
+        // TODO: check SD card and USB stick state
+
+        navigationView.setNavigationItemSelectedListener { navigationItemSelected(it) }
+    }
+
+    override fun customizeToolbar(rootView: View, toolbar: Toolbar) {
+        super.customizeToolbar(rootView, toolbar)
+
+        toolbar.setNavigationOnClickListener(null)
+
+        drawerLayout = rootView.folderShortcutsDrawerLayout
+
+        val toggle = ActionBarDrawerToggle(
+                activity, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
     }
 
 
     override fun handlesBackButtonPress(): Boolean {
         val parent = directoryContentView.currentDirectory.parentFile
 
-        if(parent == null || parent.absolutePath == "/") {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            closeDrawerLayout()
+
+            return true
+        }
+        else if(parent == null || parent.absolutePath == "/") {
             cancelSelectingFiles()
             return false
         }
@@ -70,10 +106,31 @@ class FileChooserDialog : FullscreenDialogFragment() {
         }
     }
 
+    private fun closeDrawerLayout() {
+        drawerLayout.closeDrawer(GravityCompat.START)
+    }
+
     override fun navigationButtonClicked() {
         cancelSelectingFiles()
 
         super.navigationButtonClicked()
+    }
+
+    private fun navigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.navFolderShortcutInternalStorage -> setCurrentDirectory(Environment.getExternalStorageDirectory())
+            R.id.navFolderShortcutSdCard -> setCurrentDirectory(Environment.getExternalStorageDirectory()) // TODO
+            R.id.navFolderShortcutDownloads -> setCurrentDirectory(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS))
+            R.id.navFolderShortcutCameraPhotos -> setCurrentDirectory(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM))
+            R.id.navFolderShortcutPictures -> setCurrentDirectory(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES))
+            R.id.navFolderShortcutMusic -> setCurrentDirectory(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC))
+            R.id.navFolderShortcutMovies -> setCurrentDirectory(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES))
+            R.id.navFolderShortcutDocuments -> if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
+                                                  setCurrentDirectory(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS))
+        }
+
+        closeDrawerLayout()
+        return true
     }
 
 
