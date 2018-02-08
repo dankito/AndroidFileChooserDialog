f288e66694cf4df7ca9dc2f6197625e41cfbaaec
dankl <dankl@comfylight.com>
2018-02-08 02:18:57 +0100

    Implemented showing icons for directories and files without thumbnail

--- FileChooserDialog/src/main/java/net/dankito/filechooserdialog/service/PreviewImageService.kt
+++ FileChooserDialog/src/main/java/net/dankito/filechooserdialog/service/PreviewImageService.kt
@@ -1,5 +1,6 @@
 package net.dankito.filechooserdialog.service
 
+import net.dankito.filechooserdialog.R
 import net.dankito.filechooserdialog.ui.adapter.viewholder.DirectoryContentViewHolder
 import net.dankito.filechooserdialog.ui.util.LoadThumbnailTask
 import net.dankito.filechooserdialog.ui.util.PreviewImageCache
@@ -25,11 +26,28 @@ class PreviewImageService(private val thumbnailService: ThumbnailService, privat
     }
 
     private fun getPreviewImageForFile(viewHolder: DirectoryContentViewHolder, file: File) {
-        mimeTypeService.getMimeType(file)?.let { mimeType ->
+        val mimeType = mimeTypeService.getMimeType(file)
+
+        if(mimeType == null) {
+            if(file.isDirectory) {
+                setPreviewImageToResource(viewHolder, R.drawable.ic_folder_white_48dp)
+            }
+            else {
+                setPreviewImageToResource(viewHolder, R.drawable.ic_insert_drive_file_white_48dp)
+            }
+        }
+        else {
             if(canLoadThumbnailForFile(mimeType)) {
                 LoadThumbnailTask(viewHolder, file, mimeType, thumbnailService, previewImageCache).execute()
             }
+            else {
+                setPreviewImageToResource(viewHolder, R.drawable.ic_insert_drive_file_white_48dp)
+            }
         }
+    }
+
+    private fun setPreviewImageToResource(viewHolder: DirectoryContentViewHolder, imageResourceId: Int) {
+        viewHolder.imgThumbnail.setImageResource(imageResourceId)
     }
 
     private fun canLoadThumbnailForFile(mimeType: String): Boolean {
