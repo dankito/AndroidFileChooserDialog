34b91d495e87567e45b250929cf3c5546281662a
dankl <dankito@users.noreply.github.com>
2018-07-29 16:45:48 +0200

    Extracted FolderUtils

--- FileChooserDialog/src/main/java/net/dankito/filechooserdialog/ui/view/FolderShortcutsNavigationView.kt
+++ FileChooserDialog/src/main/java/net/dankito/filechooserdialog/ui/view/FolderShortcutsNavigationView.kt
@@ -5,7 +5,6 @@ import android.content.Context
 import android.os.Build
 import android.os.Environment
 import android.support.design.widget.NavigationView
-import android.support.v4.content.ContextCompat
 import android.support.v4.view.GravityCompat
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.ActionBarDrawerToggle
@@ -15,6 +14,7 @@ import android.view.MenuItem
 import kotlinx.android.synthetic.main.file_chooser_dialog_dialog_file_chooser.view.*
 import net.dankito.filechooserdialog.R
 import net.dankito.filechooserdialog.ui.extensions.createColorStateList
+import net.dankito.filechooserdialog.ui.util.FolderUtils
 import java.io.File
 
 
@@ -28,6 +28,8 @@ class FolderShortcutsNavigationView @JvmOverloads constructor(
     private lateinit var drawerLayout: DrawerLayout
 
     private var sdCardDirectory: File? = null
+
+    private val folderUtils = FolderUtils(context)
 
 
     init {
@@ -82,77 +84,10 @@ class FolderShortcutsNavigationView @JvmOverloads constructor(
 
 
     private fun setExternalStorageMenuItems() {
-        val externalStorageDirectories = getExternalStorageDirectories()
-
-        externalStorageDirectories.forEach { directory ->
-            if(isMounted(directory)) {
-                if(isSdCard(directory)) {
-                    sdCardDirectory = directory // TODO: can there ever be more than one SD card?
-                    this.menu?.findItem(R.id.navFolderShortcutSdCard)?.isVisible = true
-                }
-            }
+        folderUtils.findSdCardDirectory()?.let {
+            sdCardDirectory = it
+            this.menu?.findItem(R.id.navFolderShortcutSdCard)?.isVisible = true
         }
-    }
-
-    private fun getExternalStorageDirectories(): List<File> {
-        val externalStorages = ContextCompat.getExternalFilesDirs(context, null)
-        val externalStorageDirectories = externalStorages.map { getRootOfDirectory(it) }.filterNotNull().toMutableSet() // to set to avoid duplicates
-
-        // for older Android devices
-        System.getenv("SECONDARY_STORAGE")?.let { secondaryStorageValue ->
-            val secondaryStoragePaths = secondaryStorageValue.split(':').map { it.replace(":", "") }
-
-            secondaryStoragePaths.forEach { secondaryStoragePath ->
-                val file = File(secondaryStoragePath)
-                if(file.exists() && file.isDirectory && file.listFiles() != null) {
-                    externalStorageDirectories.add(file)
-                }
-            }
-        }
-
-        return externalStorageDirectories.toList()
-    }
-
-    private fun isMounted(directory: File): Boolean {
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
-            @Suppress("DEPRECATION")
-            return Environment.getStorageState(directory) == Environment.MEDIA_MOUNTED
-        }
-        else if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            return Environment.getExternalStorageState(directory) == Environment.MEDIA_MOUNTED
-        }
-
-        return true // TODO: what to return as fallback value for Android versions below KitKat?
-    }
-
-    private fun isSdCard(directory: File): Boolean {
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            val isRemovable = Environment.isExternalStorageRemovable(directory)
-            val isEmulated = Environment.isExternalStorageEmulated(directory)
-
-            if(isRemovable && isEmulated == false) { // an SD card
-                return true
-            }
-
-            return false
-        }
-        else {
-            return Environment.getExternalStorageDirectory() != directory
-        }
-    }
-
-    private fun getRootOfDirectory(file: File?): File? {
-        try {
-            // Path is in format /storage.../Android....
-            // Get everything before /Android
-            if(file != null) {
-                return File(file.path.split("/Android")[0])
-            }
-        } catch(e: Exception) {
-            // TODO: log error
-        }
-
-        return null
     }
 
 
@@ -172,7 +107,7 @@ class FolderShortcutsNavigationView @JvmOverloads constructor(
             R.id.navFolderShortcutInternalStorage -> folderShortcutSelected(Environment.getExternalStorageDirectory())
             R.id.navFolderShortcutSdCard -> sdCardDirectory?.let { folderShortcutSelected(it) }
             R.id.navFolderShortcutDownloads -> folderShortcutSelected(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS))
-            R.id.navFolderShortcutCameraPhotos -> folderShortcutSelected(getCameraPhotosDirectory())
+            R.id.navFolderShortcutCameraPhotos -> folderShortcutSelected(folderUtils.getCameraPhotosDirectory())
             R.id.navFolderShortcutPictures -> folderShortcutSelected(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES))
             R.id.navFolderShortcutMusic -> folderShortcutSelected(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC))
             R.id.navFolderShortcutMovies -> folderShortcutSelected(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES))
@@ -182,30 +117,6 @@ class FolderShortcutsNavigationView @JvmOverloads constructor(
 
         closeDrawerLayout()
         return true
-    }
-
-    /**
-     * Tries to find Camera folder in DCIM dir
-     */
-    private fun getCameraPhotosDirectory(): File {
-        val dcimDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM)
-
-        var cameraInDcimDirTry = File(dcimDir, "Camera")
-        if(cameraInDcimDirTry.exists()) {
-            return cameraInDcimDirTry
-        }
-
-        cameraInDcimDirTry = File(dcimDir, "CAMERA")
-        if(cameraInDcimDirTry.exists()) {
-            return cameraInDcimDirTry
-        }
-
-        cameraInDcimDirTry = File(dcimDir, "camera")
-        if(cameraInDcimDirTry.exists()) {
-            return cameraInDcimDirTry
-        }
-
-        return dcimDir
     }
 
     private fun folderShortcutSelected(directory: File) {
